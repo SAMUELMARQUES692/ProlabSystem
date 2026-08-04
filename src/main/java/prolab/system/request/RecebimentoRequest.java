52ce974a4a62b195d0822eff1b0bba90364eadf0
samuel <samue@Samuel>
2026-08-04 17:05:08 -0300

    feat: Cria uma classe de testes para o mapper RecebimentoMapper

--- src/main/java/prolab/system/request/RecebimentoRequest.java
+++ src/main/java/prolab/system/request/RecebimentoRequest.java
@@ -2,10 +2,12 @@ package prolab.system.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Builder
 public record RecebimentoRequest(
         @NotNull
         Long agendamentoId,
---
+++ src/test/java/prolab/system/mapper/RecebimentoMapperTest.java
@@ -0,0 +1,75 @@
+package prolab.system.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import prolab.system.entity.Recebimento;
+import prolab.system.request.RecebimentoRequest;
+import prolab.system.response.RecebimentoResponse;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecebimentoMapperTest {
+
+    private final RecebimentoMapper mapper = Mappers.getMapper(RecebimentoMapper.class);
+
+    @Test
+    void toRecebimento() {
+        RecebimentoRequest request = RecebimentoRequest.builder()
+                .dataHoraRecebimento(LocalDateTime.now())
+                .pesoConferido(null)
+                .observacoes("Observacao")
+                .build();
+
+        Recebimento recebimento = mapper.toRecebimento(request);
+
+        assertNotNull(recebimento);
+
+        assertEquals(request.dataHoraRecebimento(), recebimento.getDataHoraRecebimento());
+        assertEquals(request.pesoConferido(), recebimento.getPesoConferido());
+        assertEquals(request.observacoes(), recebimento.getObservacoes());
+    }
+
+    @Test
+    void toRecebimentoResponse() {
+        Recebimento recebimento = Recebimento.builder()
+                .id(1L)
+                .dataHoraRecebimento(LocalDateTime.now())
+                .pesoConferido(null)
+                .observacoes("Observacao")
+                .build();
+
+        RecebimentoResponse response = mapper.toRecebimentoResponse(recebimento);
+
+        assertNotNull(response);
+
+        assertEquals(recebimento.getId(), response.id());
+        assertEquals(recebimento.getDataHoraRecebimento(), response.dataHoraRecebimento());
+        assertEquals(recebimento.getPesoConferido(), response.pesoConferido());
+        assertEquals(recebimento.getObservacoes(), response.observacoes());
+
+    }
+
+    @Test
+    void atualizarRecebimento() {
+        RecebimentoRequest request = RecebimentoRequest.builder()
+                .dataHoraRecebimento(LocalDateTime.now())
+                .pesoConferido(null)
+                .observacoes("Observacao")
+                .build();
+
+        Recebimento recebimento = Recebimento.builder()
+                .id(1L)
+                .dataHoraRecebimento(LocalDateTime.now())
+                .pesoConferido(null)
+                .observacoes("Observacao")
+                .build();
+
+        mapper.atualizarRecebimento(request, recebimento);
+
+        assertEquals(request.dataHoraRecebimento(), recebimento.getDataHoraRecebimento());
+        assertEquals(request.pesoConferido(), recebimento.getPesoConferido());
+        assertEquals(request.observacoes(), recebimento.getObservacoes());
+    }
+}
