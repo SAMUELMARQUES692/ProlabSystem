931c5061fe9cf50b6119a6fe34fe2e591dd637a9
samuel <samue@Samuel>
2026-08-04 13:25:33 -0300

    fix: Adiciona mais um campo na responta de createdAt no ClienteRespose

--- src/main/java/prolab/system/response/ClienteResponse.java
+++ src/main/java/prolab/system/response/ClienteResponse.java
@@ -1,11 +1,14 @@
 package prolab.system.response;
 
+import java.time.LocalDateTime;
+
 public record ClienteResponse(
         Long id,
         String razaoSocial,
         String cnpj,
         String contato,
         String endereco,
-        Boolean ativo
+        Boolean ativo,
+        LocalDateTime createdAt
 ) {
 }
